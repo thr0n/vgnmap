a63099f3a7dc396a69df5b9ee1f1532a72da198d
Hendrik Bölcke <hendrik.thorun@gmail.com>
2023-01-06 07:16:57 +0100

    fix: Fix icon positioning

--- src/components/RestaurantMap.svelte
+++ src/components/RestaurantMap.svelte
@@ -18,7 +18,7 @@
       'https://img.icons8.com/external-smashingstocks-glyph-smashing-stocks/132/external-burger-food-smashingstocks-glyph-smashing-stocks.png',
     iconSize: [22, 22], // size of the icon
     shadowSize: [50, 64], // size of the shadow
-    iconAnchor: [22, 94], // point of the icon which will correspond to marker's location
+    iconAnchor: [0, 0], // point of the icon which will correspond to marker's location
     shadowAnchor: [4, 62], // the same for the shadow
     popupAnchor: [-3, -76] // point from which the popup should open relative to the iconAnchor
   });
